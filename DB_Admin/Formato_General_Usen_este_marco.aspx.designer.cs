80f14e28cbffd5f86735b917d73c5c195c5df6b7
Davidcpu <daviddurandz67@gmail.com>
2021-08-06 12:23:30 -0600

    Create Formato_General_Usen_este_marco.aspx.designer.cs

---
+++ DB_Admin/Formato_General_Usen_este_marco.aspx.designer.cs
@@ -0,0 +1,17 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace DB_Admin
+{
+
+
+    public partial class Formato_General_Usen_este_marco
+    {
+    }
+}
